7c11cfc82dadabadac0fafb651bcd71a59ab107c
Billy Pamanian <itisbilly@gmail.com>
2025-04-18 00:38:46 -0700

    Learning Classes/object

--- index.php
+++ index.php
@@ -2,36 +2,68 @@
 
 declare(strict_types=1);
 
-?>
-
-<head>
-  <link rel="stylesheet" href="css/pico.min.css" />
-</head>
-
-<?php
-
+include("./template/top.html");
 include("./router.php");
 
 $myVar = "It works!";
 
 get("/", function () {
-  echo "<h1> You are here! </h1>";
+  include("./template/test.html");
 });
 
 get("/test", function () {
   $name = $_GET["name"] ?? null;
   $age = $_GET["age"] ?? null;
+  $person = new Persons($name, $age);
   $myVar = "Generic variable";
 ?>
   <p>
-    <?php
-    echo $myVar;
-    ?>
+    <?php echo $myVar; ?>
+  </p>
   <p>
-    <?php
-    echo "Name {$name}";
-    ?>
+    <?php echo "Name: {$person->getName()}"; ?>
+  </p>
   <p>
-  <?php
-  echo "Age {$age}";
+    <?php echo "Age: {$person->getAge()}"; ?>
+  </p>
+<?php
 });
+
+/**
+ * I'm learning how to work with classes here
+ *
+ * @param string | null $name The name of the person
+ * @param string $age The age of the person
+ * @return string person object
+ */
+class Persons
+{
+  private string $name;
+  private float $age;
+  public function __construct(string | null $name, string | null $age)
+  {
+    if ($name != null) {
+      $this->name = $name;
+    } else {
+      $this->name = "J. Doe";
+    }
+
+    if ($age != null) {
+      $this->age = intval($age);
+    } else {
+      $this->age = 99;
+    }
+  }
+
+  public function getAge(): string
+  {
+    return "{$this->age}";
+  }
+
+  public function getName(): string
+  {
+    return $this->name;
+  }
+}
+
+include("./template/bottom.html");
--- product.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "well this guy stinks!";
