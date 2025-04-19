258e933f28ab364de7b2e0b663e207360b3f20e0
Billy Pamanian <itisbilly@gmail.com>
2025-04-19 00:02:34 -0700

    Got rid of everything, starting API routing

--- index.php
+++ index.php
@@ -2,68 +2,22 @@
 
 declare(strict_types=1);
 
-include("./template/top.html");
 include("./router.php");
 
-$myVar = "It works!";
-
 get("/", function () {
-  include("./template/test.html");
+  include("./template/top.html");
+?>
+  <p>I'm outside of the php block</p>
+<?php
+  include("./template/bottom.html");
 });
 
 get("/test", function () {
-  $name = $_GET["name"] ?? null;
-  $age = $_GET["age"] ?? null;
-  $person = new Persons($name, $age);
-  $myVar = "Generic variable";
+  include("./template/top.html");
 ?>
-  <p>
-    <?php echo $myVar; ?>
-  </p>
-  <p>
-    <?php echo "Name: {$person->getName()}"; ?>
-  </p>
-  <p>
-    <?php echo "Age: {$person->getAge()}"; ?>
-  </p>
+  <p>Test function</p>
 <?php
+  include("./template/bottom.html");
 });
 
-/**
- * I'm learning how to work with classes here
- *
- * @param string | null $name The name of the person
- * @param string $age The age of the person
- * @return string person object
- */
-class Persons
-{
-  private string $name;
-  private float $age;
-  public function __construct(string | null $name, string | null $age)
-  {
-    if ($name != null) {
-      $this->name = $name;
-    } else {
-      $this->name = "J. Doe";
-    }
-
-    if ($age != null) {
-      $this->age = intval($age);
-    } else {
-      $this->age = 99;
-    }
-  }
-
-  public function getAge(): string
-  {
-    return "{$this->age}";
-  }
-
-  public function getName(): string
-  {
-    return $this->name;
-  }
-}
-
-include("./template/bottom.html");
+any("/404", "./views/404.php");
---
+++ template/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link href="css/pico.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <p>Get dat cash!</p>
+</body>
+
+</html>
